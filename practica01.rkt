84d7d233dc83c2e5a7f5d65d5a016ee7875f8d90
foalster <foalster@gmail.com>
2015-09-02 22:23:54 -0500

    Update practica01.rkt

--- practica01.rkt
+++ practica01.rkt
@@ -6,11 +6,11 @@
       [(zero? m) 1]
       [else (* n (pow n (- m 1)))])))
 
-;(test (pow 100000 0) 1)
-;(test (pow 100000 1) 100000)
-;(test (pow 5 2) 25)
-;(test (pow 1 100) 1)
-;(test (pow 5 5) 3125)
+(test (pow 100000 0) 1)
+(test (pow 100000 1) 100000)
+(test (pow 5 2) 25)
+(test (pow 1 100) 1)
+(test (pow 5 5) 3125)
 
 ;Ejercicio 02.- Función average
 ;Implementamos las funciones auxiliares de sumaElementos y numElementos para sacar el promedio
@@ -32,11 +32,11 @@
       [(null? lista) 0]
       [else (/ (sumaElementos lista) (numElementos lista))])))
 
-;(test (average '()) 0)
-;(test (average '(10)) 10)
-;(test (average '(10 10 10 20 20 20)) 15)
-;(test (average '(1 2 3 4 5 6 7 8 9 10)) 5.5)
-;(test (average '(1 1 1 1 1 1 1 1 1 1 1 1)) 1)
+(test (average '()) 0)
+(test (average '(10)) 10)
+(test (average '(10 10 10 20 20 20)) 15)
+(test (average '(1 2 3 4 5 6 7 8 9 10)) 5.5)
+(test (average '(1 1 1 1 1 1 1 1 1 1 1 1)) 1)
 
 ;Ejercicio 03.- Función primes
 ;Implementamos las funciones auxiliares isPrime, primAux y reverse, que son las que hacen que funcion la funcion primes
@@ -64,11 +64,11 @@
       [(null? lista) acc]
       [else (reverse (cdr lista) (cons (car lista) acc))])))
 
-;(test (primes 30) '(2 3 5 7 11 13 17 19 23 29))
-;(test (primes 11) '(2 3 5 7 11))
-;(test (primes 1) '())
-;(test (primes 2) '(2))
-;(test (primes 50) '(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47))
+(test (primes 30) '(2 3 5 7 11 13 17 19 23 29))
+(test (primes 11) '(2 3 5 7 11))
+(test (primes 1) '())
+(test (primes 2) '(2))
+(test (primes 50) '(2 3 5 7 11 13 17 19 23 29 31 37 41 43 47))
 
 ;Ejercicio 04.- Función zip
 (define zip
@@ -78,11 +78,11 @@
       [(null? lista2) '()]
       [else (cons (list (first lista1) (first lista2)) (zip (cdr lista1) (cdr lista2)))])))
 
-;(test (zip '(1 2) '(3 4)) '((1 3) (2 4)))
-;(test (zip '(1 2 3) '()) '())
-;(test (zip '() '(4 5 6)) '())
-;(test (zip '(8 9) '(3 2 1 4)) '((8 3) (9 2)))
-;(test (zip '(8 9 1 2) '(3 4)) '((8 3) (9 4)))      
+(test (zip '(1 2) '(3 4)) '((1 3) (2 4)))
+(test (zip '(1 2 3) '()) '())
+(test (zip '() '(4 5 6)) '())
+(test (zip '(8 9) '(3 2 1 4)) '((8 3) (9 2)))
+(test (zip '(8 9 1 2) '(3 4)) '((8 3) (9 4)))      
 
 ;Ejercicio 05.- Función reduce
 (define reduce (lambda (funcion lista base)
@@ -91,11 +91,11 @@
                    [(eqv? (numElementos lista) 1) (first lista)]
                    [else (funcion (first lista) (reduce funcion (cdr lista) base))])))
 
-;(test (reduce + '(1 2 3 4 5 6 7 8 9 10) 0) 55)
-;(test (reduce zip '((1 2 3) (4 5 6) (7 8 9)) '()) '((1 (4 7)) (2 (5 8)) (3 (6 9))))
-;(test (reduce * '(1 2 3 4 5) 0) 120)
-;(test (reduce + '(10 10 10 10 10 10 10 10 10 10) 0) 100)
-;(test (reduce mconcat '(() (1 2) (a b)) '()) '(1 2 a b))
+(test (reduce + '(1 2 3 4 5 6 7 8 9 10) 0) 55)
+(test (reduce zip '((1 2 3) (4 5 6) (7 8 9)) '()) '((1 (4 7)) (2 (5 8)) (3 (6 9))))
+(test (reduce * '(1 2 3 4 5) 0) 120)
+(test (reduce + '(10 10 10 10 10 10 10 10 10 10) 0) 100)
+(test (reduce mconcat '(() (1 2) (a b)) '()) '(1 2 a b))
 
 ;Sección II
 ;Ejercicio 06.- Función mconcat
@@ -105,11 +105,11 @@
       [(null? lista1) lista2]
       [else (cons (car lista1) (mconcat (cdr lista1) lista2))])))
 
-;(test (mconcat '(1 2 3) '(4 5 6)) '(1 2 3 4 5 6))
-;(test (mconcat '() '(4 5 6)) '(4 5 6))
-;(test (mconcat '(1 2 3) '()) '(1 2 3))
-;(test (mconcat '() '()) '())
-;(test (mconcat '(1 2 3 4) '(a b c d)) '(1 2 3 4 a b c d))
+(test (mconcat '(1 2 3) '(4 5 6)) '(1 2 3 4 5 6))
+(test (mconcat '() '(4 5 6)) '(4 5 6))
+(test (mconcat '(1 2 3) '()) '(1 2 3))
+(test (mconcat '() '()) '())
+(test (mconcat '(1 2 3 4) '(a b c d)) '(1 2 3 4 a b c d))
 
 ;Ejercicio 07.- Función mmap
 (define mmap
@@ -117,16 +117,26 @@
     (cond
       [(null? lista) '()]
       [else (cons (funcion (car lista)) (mmap funcion (cdr lista)))])))
-;(test (mmap add1 '(1 2 3 4)) '(2 3 4 5))
-;(test (mmap car '((1 2 3) (4 5 6) (7 8 9))) '(1 4 7))
-;(test (mmap cdr '((1 2 3) (4 5 6) (7 8 9))) '((2 3) (5 6) (8 9)))
+(test (mmap add1 '(1 2 3 4)) '(2 3 4 5))
+(test (mmap car '((1 2 3) (4 5 6) (7 8 9))) '(1 4 7))
+(test (mmap cdr '((1 2 3) (4 5 6) (7 8 9))) '((2 3) (5 6) (8 9)))
+(test (mmap add1 '()) '())
+(test (mmap add1 '(1000000)) '(1000001))
 
 ;Ejercicio 08.- Función mfilter
-;(define mfilter
-;  (lambda (predicate lista)
-;    (cond
-;      [(null? lista) '()]
-;      )))
+(define mfilter
+  (lambda (fun lista)
+    (cond
+      [(null? lista) '()]
+      [(fun (car lista)) (cons (car lista ) (mfilter fun (cdr lista)))]
+      [else (mfilter fun (cdr lista))])))
+
+;;Pruebas con la funcion mfilter
+(test (mfilter (lambda (x) (not (zero? x))) '(2 0 1 4 0)) '(2 1 4))
+(test (mfilter (lambda (l) (not (empty? l))) '((1 4 2) () (2 4) ())) '((1 4 2) (2 4)))
+(test (mfilter (lambda (n) (= (modulo n 2) 0)) '(1 2 3 4 5 6)) '(2 4 6))
+(test (mfilter (lambda (k) (not (number? k))) '()) '())
+(test (mfilter (lambda (a) (zero? a)) '(1 2 3 4 0 12)) '(0))
 
 ;Ejercicio 09.- Función any? y pruebas
 (define any?
@@ -135,11 +145,12 @@
       [(null? lista) #f]
       [(propocision (first lista)) #t]
       [else (any? propocision (cdr lista))])))
-;(test (any? number? '()) #f)
-;(test (any? number? '(a b c d 1)) #t)
-;(test (any? symbol? '(1 2 3 4)) #f)
-;(test (any? symbol? '(1 2 3 4 q 1 2)) #t)
-;(test (any? number? '(a b c 0 d e f)) #t)
+      
+(test (any? number? '()) #f)
+(test (any? number? '(a b c d 1)) #t)
+(test (any? symbol? '(1 2 3 4)) #f)
+(test (any? symbol? '(1 2 3 4 q 1 2)) #t)
+(test (any? number? '(a b c 0 d e f)) #t)
 
 ;Ejercicio 10.- Función every? y pruebas
 (define every?
@@ -148,11 +159,11 @@
       [(null? lista) #t]
       [(propocision (first lista)) (every? propocision (cdr lista))]
       [else #f])))
-;(test (every? number? '()) #t)
-;(test (every? number? '(1 2 3)) #t)
-;(test (every? number? '(1 2 3 a)) #f)
-;(test (every? symbol? '(1 2 3 a)) #f)
-;(test (every? symbol? '(a b c d e f g h)) #t)
+(test (every? number? '()) #t)
+(test (every? number? '(1 2 3)) #t)
+(test (every? number? '(1 2 3 a)) #f)
+(test (every? symbol? '(1 2 3 a)) #f)
+(test (every? symbol? '(a b c d e f g h)) #t)
 
 ;Ejercicio 11.- Función mpowerset
 
@@ -161,3 +172,10 @@
     (cond
       [(null? lista) '(())]
       [else (let ((cdr (mpowerset (cdr lista)))) (mconcat (mmap ( lambda (listatemp) (cons (car lista) listatemp)) cdr) cdr))])))
+
+;;Pruebas de la función mpowerset
+(test (mpowerset '()) '(()))
+(test (mpowerset '(1)) '((1) ()))
+(test (mpowerset '(1 2)) '((1 2) (1) (2) ()))
+(test (mpowerset '(1 2 3)) '((1 2 3) (1 2) (1 3) (1) (2 3) (2) (3) ()))
+(test (mpowerset '((1 2 3) 1 2 3)) '(((1 2 3) 1 2 3) ((1 2 3) 1 2) ((1 2 3) 1 3) ((1 2 3) 1) ((1 2 3) 2 3) ((1 2 3) 2) ((1 2 3) 3) ((1 2 3)) (1 2 3) (1 2) (1 3) (1) (2 3) (2) (3) ()))
