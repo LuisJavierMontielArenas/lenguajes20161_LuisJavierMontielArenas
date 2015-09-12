81317944b6101d31ebfff084e1df688bbc0cb8f8
foalster <foalster@gmail.com>
2015-09-12 11:56:16 -0500

    Update practica02.rkt

--- practica02.rkt
+++ practica02.rkt
@@ -103,13 +103,34 @@
     (cond
       [(MEmpty? lis) (MEmpty)]
       [else (MCons (funcion(MCons-n lis)) (mapML funcion(MCons-lst lis)))])))
-             
+
 ;;12.- Definir filterML
 (define filterML
   (lambda (f l)
     (cond
       [(MEmpty? l) (MEmpty)]
       [(if (f(MCons-n l)) (MCons (MCons-n l) (filterML f (MCons-lst l))) (filterML f (MCons-lst l)))])))
+
+(define-type Coordinates
+  [GPS (lat number?)
+       (long number?)])
+
+(define-type Location
+  [building (name string?)
+(loc GPS?)])
+
+;; Coordenadas GPS
+(define gps-satelite (GPS 19.510482 -99.23411900000002))
+(define gps-ciencias (GPS 19.3239411016 -99.179806709))
+(define gps-zocalo (GPS 19.432721893261117 -99.13332939147949))
+(define gps-perisur (GPS 19.304135 -99.19001000000003))
+ 
+(define plaza-satelite (building "Plaza Satelite" gps-satelite))
+(define ciencias (building "Facultad de Ciencias" gps-ciencias))
+(define zocalo (building "Zocalo" gps-zocalo))
+(define plaza-perisur (building "Plaza Perisur" gps-perisur))
+
+(define plazas (MCons plaza-satelite (MCons plaza-perisur (MEmpty))))
 
 ;;17.- Definir area
 (define (area figure)
@@ -120,6 +141,8 @@
             (* l l)]
     [Rectangle (e a l)
                (* a l)]))
+;;Definir gps-coordinates
+
 
 ;;18.- Definir in-figure?
 (define (in-figure? fig point)
