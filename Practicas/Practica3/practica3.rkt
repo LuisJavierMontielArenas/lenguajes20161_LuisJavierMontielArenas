a755fca6b38e8d49981d31e533fe28d86488d550
LuisJavierMontielArenas <luis.javier@ciencias.unam.mx>
2015-09-20 03:07:01 -0500

    Update practica3.rkt

--- Practicas/Practica3/practica3.rkt
+++ Practicas/Practica3/practica3.rkt
@@ -11,3 +11,12 @@
              (cond
                [(and (EmptyBT? left) (EmptyBT? right)) 0]
                [else (+ 1 (+ (ninBT left) (ninBT right)))])])))
+
+;;Pruebas de la función ninBT
+(test (ninBT (EmptyBT)) 0)
+(test (ninBT (BNode < (EmptyBT) 1 (EmptyBT))) 0)
+(test (ninBT (BNode < (BNode < (EmptyBT) 3 (EmptyBT)) 1 (BNode < (EmptyBT) 2 (EmptyBT)))) 1)
+(test (ninBT (bns (bns (bns ebt "pow" ebt) "ext" (bns ebt "trial" ebt)) "typed" (bns ebt "lambda" ebt))) 2)
+(define example1 (bns (bns (bns ebt "pow" ebt) "ext" (bns ebt "trial" ebt)) "typed" (bns ebt "lambda" ebt)))
+(define bigger-example (bns example1 "functional" example1))
+(test (ninBT bigger-example) 5)
