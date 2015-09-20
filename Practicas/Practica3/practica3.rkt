1fc2ec770e0ac8095d8c9cd3dbd7adad3edbb718
LuisJavierMontielArenas <luis.javier@ciencias.unam.mx>
2015-09-20 03:19:17 -0500

    Update practica3.rkt

--- Practicas/Practica3/practica3.rkt
+++ Practicas/Practica3/practica3.rkt
@@ -20,3 +20,13 @@
 (define example1 (bns (bns (bns ebt "pow" ebt) "ext" (bns ebt "trial" ebt)) "typed" (bns ebt "lambda" ebt)))
 (define bigger-example (bns example1 "functional" example1))
 (test (ninBT bigger-example) 5)
+
+;;nlBT
+(define nlBT
+  (lambda (tree)
+    (type-case BTree tree
+      [EmptyBT () 0]
+      [BNode (comp left elem right)
+             (cond
+               [(and (EmptyBT? left) (EmptyBT? right)) 1]
+               [else (+ (nlBT left) (nlBT right))])])))
