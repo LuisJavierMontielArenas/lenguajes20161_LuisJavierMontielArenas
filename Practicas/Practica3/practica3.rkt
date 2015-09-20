c7ebca7ed82b5dd1e659ee45e4f7b24113cd85a5
LuisJavierMontielArenas <luis.javier@ciencias.unam.mx>
2015-09-20 02:51:18 -0500

    Update practica3.rkt

--- Practicas/Practica3/practica3.rkt
+++ Practicas/Practica3/practica3.rkt
@@ -1,3 +1,13 @@
 #lang plai
 
 (require "practica3-base.rkt")
+
+;;ninBT
+(define ninBT
+  (lambda (tree)
+    (type-case BTree tree
+      [EmptyBT () 0]
+      [BNode (comp left elem right) 
+             (cond
+               [(and (EmptyBT? left) (EmptyBT? right)) 0]
+               [else (+ 1 (+ (ninBT left) (ninBT right)))])])))
